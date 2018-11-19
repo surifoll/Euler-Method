aec22e9ef056dd2d06bca2daad57715263677f53
suraj.fehintola <suraj.fehintola@wemabank.com>
2018-11-19 17:33:55 +0100

    [Suraj]

--- src/ConsoleApp3/Program.cs
+++ src/ConsoleApp3/Program.cs
@@ -1,25 +1,26 @@
 ﻿using System;
 using System.Linq;
 
-namespace ConsoleApp3
+namespace ConsoleApp2
 {/// <summary>
-/// Improved Euler Method
-/// </summary>
+    /// Euler Method
+    /// </summary>
     public class Program
     {
         /// <summary>
+        /// 
         /// Fehintola Suraj Ayodeji
-        ///  
-        ///  
+        /// 
+        /// 
+        /// 
         /// </summary>
         public void Main(string[] args)
         {
-            // ================== Input ===================
-            var x = new double[] { 0.5, 0.5, 0.5, 0.5 };
-            var y = new double[4]; var y0 = new double[4];double yII2 = 0.0; var lamda = 0.0; var n = 0;
-            var A = new[] { new double[] { 10, 7, 8, 7 }, new double[] { 7, 5, 6, 5 }, new double[] { 8, 6, 10, 9 }, new double[] { 7, 5, 9, 10 } };
-            //============    Input ==================
-
+            var x = new double[] { 1, 1, 1 };
+            var y = new double[3]; var y0 = new double[3];
+            var lamda = Double.MinValue;
+            var n = 0;
+            var A = new[] { new double[] { 5, -1, 7 }, new double[] { -1, -1, 1 }, new double[] { 7, 1, 5 } };
             while (n < 10)
             {
                 for (var i = 0; i < A.Length; i++)
@@ -32,16 +33,14 @@ namespace ConsoleApp3
                 }
                 for (var i = 0; i < A.Length; i++)
                 {
-                    lamda += y[i]*x[i];
-                    yII2 += y[i] * y[i];
+                    lamda = (lamda < Math.Abs(y[i])) ? Math.Abs(y[i]) : lamda;
                 }
                 for (var i = 0; i < A.Length; i++)
                 {
-                    x[i] = y[i] / Math.Sqrt(yII2);
+                    x[i] = y[i] / lamda;
                 }
-                
                 Console.WriteLine("###########################################");
-                Console.WriteLine($" iteration {n + 1} --- x0 = ({x[0]}), x1 = ({x[1]}) x2 = ({x[2]})   ----- lamda = {lamda} -----  ||Y||2 = {Math.Sqrt(yII2)}   ");
+                Console.WriteLine($" iteration {n + 1} --- x0 ({x[0]}), x1 ({x[1]}) x2 ({x[2]})   ----- lamda {lamda} -----");
                 Console.WriteLine("###########################################");
                 n++; lamda = 0;
             }
